53d2029feb1c58952a440f27a45487b1e3392f34
Fcayorr <38299205+FCayor@users.noreply.github.com>
2018-09-21 12:16:44 -0500

    Add files via upload
    heap extract and max heap

---
+++ four_card.cpp
@@ -0,0 +1,27 @@
+// Example program
+#include <iostream>
+#include <string>
+#define N=10
+#define INF=111111111111111
+
+int main()
+{
+  std::string name;
+  std::cout << "What is your name? ";
+  getline (std::cin, name);
+  std::cout << "Hello, " << name << "!\n";
+}
+
+int Heap_Extract_Max(int A[]){
+    int max=A[1];
+    A[1]=A[heap_size];
+    heap_size=heap_size-1;
+    max_heapify(A,1);
+    return max;
+    }
+    
+void Max_Heap_Insert(int A[],int key){
+    heap_size=heap_size+1;
+    A[heap_size]=INF;
+    Heap_Increase_Key(A,heap_size,key);
+    }
